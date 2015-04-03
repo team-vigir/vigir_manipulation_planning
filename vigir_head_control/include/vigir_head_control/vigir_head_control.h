222d8bc1ce3a44df1dedc51b03df597be5ec24ae
Florian Reimold <reimold@sim.informatik.tu-darmstadt.de>
2015-04-03 17:00:52 +0200

    Head Controler can switch between tracking modes and setting direct joints is working. Computing correct joints for tracking left/right hand does not work.

--- vigir_head_control/include/vigir_head_control/vigir_head_control.h
+++ vigir_head_control/include/vigir_head_control/vigir_head_control.h
@@ -4,20 +4,35 @@
 #include <ros/ros.h>
 #include <vigir_planning_msgs/HeadControlCommand.h>
 #include <trajectory_msgs/JointTrajectory.h>
+#include <geometry_msgs/PoseStamped.h>
 
 namespace head_control{
-    class HeadControl {
 
+    class HeadControl {
 
     public:
       HeadControl();
       virtual ~HeadControl();
     protected:
       void HeadControlCb(const vigir_planning_msgs::HeadControlCommand &command);
+      void trackLeftHandCb(const geometry_msgs::PoseStamped &pose);
+      void trackRightHandCb(const geometry_msgs::PoseStamped &pose);
+
+      void setHeadJointPosition(const double pan, const double tilt);
+      std::vector<double> computeJointsLeftHandTracking();
+      std::vector<double> computeJointsRightHandTracking();
     private:
+      unsigned char tracking_mode;
       ros::Publisher joint_trajectory_pub;
       ros::Subscriber head_control_sub;
-
+      ros::Subscriber left_hand_sub;
+      ros::Subscriber right_hand_sub;
     };
+}
+
+namespace head_tracking_mode{
+  const unsigned char NONE = 0;
+  const unsigned char LEFT_HAND_TRACKING = 1;
+  const unsigned char RIGHT_HAND_TRACKING = 2;
 }
 #endif // VIGIR_HEAD_CONTROL_NODE_H
