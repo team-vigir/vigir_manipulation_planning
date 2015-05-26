d141de1c6f79c83ffcca57c54c70941cfa2efef7
Martin Oehler <martin.sven.oehler@gmail.com>
2015-05-26 15:22:11 -0400

    head control throttled down to 30hz status updates instead of spam

--- vigir_head_control/src/vigir_head_control.cpp
+++ vigir_head_control/src/vigir_head_control.cpp
@@ -13,52 +13,56 @@ namespace head_control{
         pnh_.param("track_frame_threshold", track_frame_threshold,0.01);
         head_control_sub = nh_.subscribe("/thor_mang/head_control_mode", 1, &HeadControl::HeadControlCb, this);
 
-        tf_sub = nh_.subscribe("/tf", 10, &HeadControl::tfCb, this);
-
-        left_hand_sub = nh_.subscribe("/flor/l_arm_current_pose", 1, &HeadControl::trackLeftHandCb, this);
-        right_hand_sub = nh_.subscribe("/flor/r_arm_current_pose", 1, &HeadControl::trackRightHandCb, this);
-
         joint_trajectory_pub = nh_.advertise<trajectory_msgs::JointTrajectory>("/thor_mang/head_traj_controller/command", 0, false);
 
         old_target_frame_origin.setZero();
-
+        head_cmd.resize(2, 0);
     }
 
-    HeadControl::~HeadControl()
-    {}
+    void HeadControl::updateHeadPosition() {
+      if(tracking_mode == vigir_planning_msgs::HeadControlCommand::TRACK_FRAME){
+          std::vector<double> joints = computeJointsForTracking(tracking_frame);
+          if (joints.size() < 2) return;
+          setHeadJointPosition(joints[0], joints[1]);
+      } else if (tracking_mode == vigir_planning_msgs::HeadControlCommand::TRACK_LEFT_HAND) {
+          std::vector<double> joints = computeJointsForTracking("l_hand");
+          if (joints.size() < 2) return;
+          setHeadJointPosition(joints[0], joints[1]);
+      } else if (tracking_mode == vigir_planning_msgs::HeadControlCommand::TRACK_RIGHT_HAND) {
+          std::vector<double> joints = computeJointsForTracking("r_hand");
+          if (joints.size() < 2) return;
+          setHeadJointPosition(joints[0], joints[1]);
+      } else if (tracking_mode == vigir_planning_msgs::HeadControlCommand::USE_PROVIDED_JOINTS) {
+          setHeadJointPosition(head_cmd[0], head_cmd[1]);
+      }
+    }
 
-
-    void HeadControl::HeadControlCb(const vigir_planning_msgs::HeadControlCommand &command){
-     ///flor/l_arm_current_pose
+    void HeadControl::HeadControlCb(const vigir_planning_msgs::HeadControlCommand &command) {
      /// //trajectory_msgs/JointTrajectory
-        if(tracking_mode != command.motion_type) {
+        if (tracking_mode != command.motion_type) {
         //ROS_INFO("Setting Head Control Mode to %u", command.motion_type);
         }
-        if(command.motion_type == vigir_planning_msgs::HeadControlCommand::NONE){
+        if (command.motion_type == vigir_planning_msgs::HeadControlCommand::NONE) {
             tracking_mode = vigir_planning_msgs::HeadControlCommand::NONE;
         }
-        else if(command.motion_type == vigir_planning_msgs::HeadControlCommand::LOOK_STRAIGHT){
+        else if (command.motion_type == vigir_planning_msgs::HeadControlCommand::LOOK_STRAIGHT) {
             tracking_mode = vigir_planning_msgs::HeadControlCommand::LOOK_STRAIGHT;
             setHeadJointPosition(0.0, 0.0);
         }
-        else if(command.motion_type == vigir_planning_msgs::HeadControlCommand::USE_PROVIDED_JOINTS){
-            // TODO: We might need a mutex here for changing the node
-            tracking_mode = vigir_planning_msgs::HeadControlCommand::NONE;
+        else if (command.motion_type == vigir_planning_msgs::HeadControlCommand::USE_PROVIDED_JOINTS) {
+            tracking_mode = vigir_planning_msgs::HeadControlCommand::USE_PROVIDED_JOINTS;
 
-            if(command.provided_joints.size() < 2){
+            if (command.provided_joints.size() < 2) {
                 ROS_WARN("Head cannot be moved because values are missing. (%lu provided)", command.provided_joints.size());
-            }else{
-                double pan = std::min(std::max(-1.57, - command.provided_joints[0]), 2.45);
-                double tilt = std::min(std::max(-1.32, command.provided_joints[1]), 0.79);
-                ROS_DEBUG("Moving Head to provided joint positions: (pan, tilt) = (%f, %f)", pan, tilt);
-                setHeadJointPosition(pan, tilt);
+            } else {
+                head_cmd[0] = std::min(std::max(-1.57, - command.provided_joints[0]), 2.45); // pan
+                head_cmd[1] = std::min(std::max(-1.32, command.provided_joints[1]), 0.79); // tilt
             }
-
-        } else if (command.motion_type == vigir_planning_msgs::HeadControlCommand::TRACK_LEFT_HAND){
+        } else if (command.motion_type == vigir_planning_msgs::HeadControlCommand::TRACK_LEFT_HAND) {
             tracking_mode = vigir_planning_msgs::HeadControlCommand::TRACK_LEFT_HAND;
-        } else if (command.motion_type == vigir_planning_msgs::HeadControlCommand::TRACK_RIGHT_HAND){
+        } else if (command.motion_type == vigir_planning_msgs::HeadControlCommand::TRACK_RIGHT_HAND) {
             tracking_mode = vigir_planning_msgs::HeadControlCommand::TRACK_RIGHT_HAND;
-        }else if (command.motion_type == vigir_planning_msgs::HeadControlCommand::TRACK_FRAME){
+        }else if (command.motion_type == vigir_planning_msgs::HeadControlCommand::TRACK_FRAME) {
             tracking_frame = command.tracking_frame;
             tracking_mode = vigir_planning_msgs::HeadControlCommand::TRACK_FRAME;
         }else{
@@ -121,46 +125,17 @@ namespace head_control{
             return std::vector<double>();
         }
 
-
-
         tf::Vector3 dir(old_target_frame_origin.x() - base_camera_transform.getOrigin().x(), old_target_frame_origin.y() - base_camera_transform.getOrigin().y(), old_target_frame_origin.z() - base_camera_transform.getOrigin().z());
 
         double pan = atan2(dir.y(), dir.x()); //yaw
         double tilt = -atan2(dir.z(), sqrt(dir.x()*dir.x() + dir.y()*dir.y()));  // pitch
-
-        std::vector<double> joints;
-
         pan = std::min(std::max(-1.57, pan), 2.45);
         tilt = std::min(std::max(-1.32, tilt), 0.79);
 
+        std::vector<double> joints;
         joints.push_back(pan);
         joints.push_back(tilt);
 
         return joints;
-    }
-
-
-    void HeadControl::tfCb(const tf2_msgs::TFMessage &tfmsg){
-        if(tracking_mode == vigir_planning_msgs::HeadControlCommand::TRACK_FRAME){
-            std::vector<double> joints = computeJointsForTracking(tracking_frame);
-            if (joints.size() < 2) return;
-            setHeadJointPosition(joints[0], joints[1]);
-        }
-    }
-
-    void HeadControl::trackLeftHandCb(const geometry_msgs::PoseStamped &pose){
-        if(tracking_mode == vigir_planning_msgs::HeadControlCommand::TRACK_LEFT_HAND){
-            std::vector<double> joints = computeJointsForTracking("l_hand");
-            if (joints.size() < 2) return;
-            setHeadJointPosition(joints[0], joints[1]);
-        }
-    }
-
-    void HeadControl::trackRightHandCb(const geometry_msgs::PoseStamped &pose){
-        if(tracking_mode == vigir_planning_msgs::HeadControlCommand::TRACK_RIGHT_HAND){
-            std::vector<double> joints = computeJointsForTracking("r_hand");
-            if (joints.size() < 2) return;
-            setHeadJointPosition(joints[0], joints[1]);
-        }
     }
 }
